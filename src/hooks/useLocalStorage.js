fd7d6f5ec7941639bd3cf1be3ddab862e156aefd
Amritanshu rai <amritanshurai13@gmail.com>
2022-09-05 10:45:07 +0530

    feat(initial-data): initial local storage added

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,13 @@
 import { useState } from 'react';
+import { sampleData } from '../sample';
+
+const initializeStorage = () => {
+  if (localStorage.length) return;
+  ["challenges", "currentUser", "allUsers"].forEach(item => localStorage.setItem(item, JSON.stringify(sampleData[item])))
+}
 
 function useLocalStorage(key, initialvalue) {
+  initializeStorage();
   const [value, setValue] = useState(() => {
     const savedValue = JSON.parse(localStorage.getItem(key));
     if (savedValue == null) {
